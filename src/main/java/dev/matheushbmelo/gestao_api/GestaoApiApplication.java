14599d8d45c1d0f08bb02c91a66b3e55d2e1e217
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-14 20:27:11 -0300

    chore: adiciona estrutura inicial do projeto

---
+++ src/main/java/dev/matheushbmelo/gestao_api/GestaoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.matheushbmelo.gestao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheushbmelo/gestao_api/GestaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheushbmelo.gestao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
