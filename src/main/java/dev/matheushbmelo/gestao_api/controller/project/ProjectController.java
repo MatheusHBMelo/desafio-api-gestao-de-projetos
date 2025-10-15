adf5dd11f76622ff0849a4877a61cffe9c984d93
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-15 10:18:54 -0300

    feat: adiciona endpoint para buscar todos os projetos

--- src/main/java/dev/matheushbmelo/gestao_api/controller/project/ProjectController.java
+++ src/main/java/dev/matheushbmelo/gestao_api/controller/project/ProjectController.java
@@ -1,14 +1,14 @@
 package dev.matheushbmelo.gestao_api.controller.project;
 
 import dev.matheushbmelo.gestao_api.controller.project.dto.ProjectRequestDto;
+import dev.matheushbmelo.gestao_api.controller.project.dto.ProjectResponseDto;
 import dev.matheushbmelo.gestao_api.service.project.ProjectService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/projects")
@@ -23,5 +23,10 @@ public class ProjectController {
     public ResponseEntity<Void> createNewProject(@RequestBody @Valid ProjectRequestDto projectRequestDto) {
         this.projectService.create(projectRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjectResponseDto>> findAllProjects() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.projectService.findAll());
     }
 }
