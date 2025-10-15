bed29b1391275b8de90f19eafc30c8ca3cb5256a
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-15 11:09:55 -0300

    feat: mapeia a entidade de tarefa

--- src/main/java/dev/matheushbmelo/gestao_api/entity/project/ProjectEntity.java
+++ src/main/java/dev/matheushbmelo/gestao_api/entity/project/ProjectEntity.java
@@ -1,10 +1,13 @@
 package dev.matheushbmelo.gestao_api.entity.project;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.matheushbmelo.gestao_api.entity.task.TaskEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_projects")
@@ -24,4 +27,7 @@ public class ProjectEntity {
     private LocalDate startDate;
     @JsonFormat(pattern = "dd/MM/yyyy ")
     private LocalDate endDate;
+
+    @OneToMany(mappedBy = "projectId", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TaskEntity> tasks = new ArrayList<>();
 }
