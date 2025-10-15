c98836438c4853be4b457a697b3d91a6eeb3ad3c
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-15 10:19:20 -0300

    feat: adiciona método que busca todos os projetos cadastrados

--- src/main/java/dev/matheushbmelo/gestao_api/service/project/ProjectService.java
+++ src/main/java/dev/matheushbmelo/gestao_api/service/project/ProjectService.java
@@ -1,6 +1,7 @@
 package dev.matheushbmelo.gestao_api.service.project;
 
 import dev.matheushbmelo.gestao_api.controller.project.dto.ProjectRequestDto;
+import dev.matheushbmelo.gestao_api.controller.project.dto.ProjectResponseDto;
 import dev.matheushbmelo.gestao_api.entity.project.ProjectEntity;
 import dev.matheushbmelo.gestao_api.mapper.project.ProjectMapper;
 import dev.matheushbmelo.gestao_api.repository.project.ProjectRepository;
@@ -8,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class ProjectService {
@@ -21,5 +23,9 @@ public class ProjectService {
         ProjectEntity newProject = ProjectMapper.INSTANCE.mapToProject(projectRequestDto);
         newProject.setStartDate(LocalDate.now());
         this.projectRepository.save(newProject);
+    }
+
+    public List<ProjectResponseDto> findAll() {
+        return ProjectMapper.INSTANCE.mapToProjectResponseDtoList(this.projectRepository.findAll());
     }
 }
