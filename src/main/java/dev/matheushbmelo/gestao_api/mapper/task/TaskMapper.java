59cd9e1e3fd10e0d61cf419d86bf34d4391fb90e
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-16 09:03:07 -0300

    feat: adiciona mapeamento entre taskEntity e TaskDto

--- src/main/java/dev/matheushbmelo/gestao_api/mapper/task/TaskMapper.java
+++ src/main/java/dev/matheushbmelo/gestao_api/mapper/task/TaskMapper.java
@@ -1,13 +1,18 @@
 package dev.matheushbmelo.gestao_api.mapper.task;
 
 import dev.matheushbmelo.gestao_api.controller.task.dto.TaskRequestDto;
+import dev.matheushbmelo.gestao_api.controller.task.dto.TaskResponseDto;
 import dev.matheushbmelo.gestao_api.entity.task.TaskEntity;
+import dev.matheushbmelo.gestao_api.mapper.project.ProjectMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = ProjectMapper.class)
 public interface TaskMapper {
-    TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
-
+    @Mapping(target = "projectId.id", source = "projectId")
     TaskEntity mapToTaskEntity(TaskRequestDto taskRequestDto);
+
+    List<TaskResponseDto> mapToTaskResponseDtoList(List<TaskEntity> tasks);
 }
