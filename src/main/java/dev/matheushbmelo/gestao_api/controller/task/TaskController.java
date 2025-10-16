b31a9341c3beb5b696ed6e4ca358dfefca778d26
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-16 09:01:40 -0300

    feat: adiciona endpoint para recuperar tarefas filtradas por parametros

--- src/main/java/dev/matheushbmelo/gestao_api/controller/task/TaskController.java
+++ src/main/java/dev/matheushbmelo/gestao_api/controller/task/TaskController.java
@@ -1,14 +1,14 @@
 package dev.matheushbmelo.gestao_api.controller.task;
 
 import dev.matheushbmelo.gestao_api.controller.task.dto.TaskRequestDto;
+import dev.matheushbmelo.gestao_api.controller.task.dto.TaskResponseDto;
 import dev.matheushbmelo.gestao_api.service.task.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/tasks")
@@ -23,5 +23,12 @@ public class TaskController {
     public ResponseEntity<Void> createTask(@RequestBody @Valid TaskRequestDto taskRequestDto) {
         this.taskService.create(taskRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDto>> findByParam(@RequestParam(required = true) String status,
+                                                             @RequestParam(required = true) String priority,
+                                                             @RequestParam(required = true) Long projectId) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.taskService.findByFilter(status, priority, projectId));
     }
 }
