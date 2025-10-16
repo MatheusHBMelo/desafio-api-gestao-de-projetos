4aeb73bb8ed84ee4f5bdd7b88bf215ee37ce920d
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-16 09:02:38 -0300

    feat: adiciona método que busca uma lista de tarefas por determinado filtro

--- src/main/java/dev/matheushbmelo/gestao_api/service/task/TaskService.java
+++ src/main/java/dev/matheushbmelo/gestao_api/service/task/TaskService.java
@@ -1,7 +1,9 @@
 package dev.matheushbmelo.gestao_api.service.task;
 
 import dev.matheushbmelo.gestao_api.controller.task.dto.TaskRequestDto;
+import dev.matheushbmelo.gestao_api.controller.task.dto.TaskResponseDto;
 import dev.matheushbmelo.gestao_api.entity.project.ProjectEntity;
+import dev.matheushbmelo.gestao_api.entity.task.Priority;
 import dev.matheushbmelo.gestao_api.entity.task.Status;
 import dev.matheushbmelo.gestao_api.entity.task.TaskEntity;
 import dev.matheushbmelo.gestao_api.mapper.task.TaskMapper;
@@ -10,22 +12,33 @@ import dev.matheushbmelo.gestao_api.repository.task.TaskRepository;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
     private final ProjectRepository projectRepository;
+    private final TaskMapper taskMapper;
 
-    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository) {
+    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
         this.projectRepository = projectRepository;
+        this.taskMapper = taskMapper;
     }
 
     public void create(@Valid TaskRequestDto taskRequestDto) {
-        TaskEntity newTask = TaskMapper.INSTANCE.mapToTaskEntity(taskRequestDto);
+        TaskEntity newTask = taskMapper.mapToTaskEntity(taskRequestDto);
         newTask.setStatus(Status.TODO);
         ProjectEntity project = this.projectRepository.findById(taskRequestDto.projectId())
                 .orElseThrow(() -> new RuntimeException("Não existe projeto com ID: " + taskRequestDto.projectId()));
         newTask.setProjectId(project);
         this.taskRepository.save(newTask);
+    }
+
+    public List<TaskResponseDto> findByFilter(String status, String priority, Long projectId) {
+        Status statusEnum = Status.valueOf(status.toUpperCase());
+        Priority priorityEnum = Priority.valueOf(priority.toUpperCase());
+        List<TaskEntity> tasks = this.taskRepository.findByFilters(statusEnum, priorityEnum, projectId);
+        return taskMapper.mapToTaskResponseDtoList(tasks);
     }
 }
