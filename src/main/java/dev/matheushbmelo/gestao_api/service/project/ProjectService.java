caa28a691159c3915b31963376c775bfeba01eac
MatheusHBMelo <matheushbmelo@gmail.com>
2025-10-22 08:25:26 -0300

    refactor: altera a forma de chamada do mapper

--- src/main/java/dev/matheushbmelo/gestao_api/service/project/ProjectService.java
+++ src/main/java/dev/matheushbmelo/gestao_api/service/project/ProjectService.java
@@ -14,18 +14,20 @@ import java.util.List;
 @Service
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
 
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, ProjectMapper projectMapper) {
         this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
     }
 
     public void create(@Valid ProjectRequestDto projectRequestDto) {
-        ProjectEntity newProject = ProjectMapper.INSTANCE.mapToProject(projectRequestDto);
+        ProjectEntity newProject = this.projectMapper.mapToProject(projectRequestDto);
         newProject.setStartDate(LocalDate.now());
         this.projectRepository.save(newProject);
     }
 
     public List<ProjectResponseDto> findAll() {
-        return ProjectMapper.INSTANCE.mapToProjectResponseDtoList(this.projectRepository.findAll());
+        return this.projectMapper.mapToProjectResponseDtoList(this.projectRepository.findAll());
     }
 }
